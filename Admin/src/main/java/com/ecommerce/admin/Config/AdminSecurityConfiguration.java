a8de158c425440f26c48393b0c5828eb80992a67
Nived Suresh <apple@macs-MacBook-Air.local>
2023-11-15 17:02:49 +0530

    try

--- Admin/src/main/java/com/ecommerce/admin/Config/AdminSecurityConfiguration.java
+++ Admin/src/main/java/com/ecommerce/admin/Config/AdminSecurityConfiguration.java
@@ -7,12 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -32,16 +28,16 @@ public class AdminSecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.ignoringRequestMatchers("/search"));
+        http.csrf(csrf-> csrf
+                .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/admin/authenticate"))
+                .ignoringRequestMatchers("/", "/product-details/**"));
 
-//        http.authorizeHttpRequests(authorize -> authorize
-//                                .requestMatchers("/static/**","/css/**", "/fonts/**", "/imgs/**","/js/**","/sass/**", "/product-images").permitAll()
-//                                .requestMatchers("/admin/send-otp","/admin/forgot-password", "admin/login",
-//                                        "/admin/send-token", "/admin/reset-password").permitAll()
-//                                .requestMatchers("/admin/forgot-password").permitAll()
-//                                .anyRequest().hasAuthority("ADMIN"))
-
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+        http.authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers("/static/**","/css/**", "/fonts/**", "/imgs/**","/js/**","/sass/**", "/product-images").permitAll()
+                                .requestMatchers("/admin/send-otp","/admin/forgot-password", "admin/login",
+                                        "/admin/send-token", "/admin/reset-password").permitAll()
+                                .requestMatchers("/admin/forgot-password").permitAll()
+                                .anyRequest().hasAuthority("ADMIN"))
 
 
                 .formLogin(formLogin -> formLogin
