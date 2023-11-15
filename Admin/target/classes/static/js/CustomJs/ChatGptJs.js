bb8a45fa7360ee5cc2ab97dfffa6d86a2593785c
Nived Suresh <apple@macs-MacBook-Air.local>
2023-11-15 17:24:51 +0530

    try

--- Admin/target/classes/static/js/CustomJs/ChatGptJs.js
+++
@@ -1,9 +0,0 @@
-
-    function removeErrorQueryParam() {
-    if (window.location.search.includes("?error")) {
-    const newUrl = window.location.pathname;
-    history.replaceState({}, "", newUrl);
-    }
-    }
-    // Call the function when the page is loaded
-    window.addEventListener("load", removeErrorQueryParam);
